2b9bcad525916ed722db08f8e904705614098098
Miikka-K <51471@edu.raseko.fi>
2024-02-06 14:29:33 +0200

    Added a constructor with one, two and three arguments (3 overloads)

--- LuokkaHarjoitus1/Program.cs
+++ LuokkaHarjoitus1/Program.cs
@@ -8,10 +8,36 @@ namespace LuokkaHarjoitus1
 {
     class Hooman
     {
-        // Define properties of Hooman
-        string name;
-        int age;
-        string gender;
+        // Define properties of Hooman ie. fields
+        string name = "Essi Esimerkki";
+        int age = 30;
+        string gender = "Emäntä";
+
+        Hooman()
+        {
+
+        }
+
+        // Constructor with one argument
+        Hooman(string name)
+        {
+            this.name = name;
+        }
+
+        // Constructor with two arguments
+        Hooman(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        // Constructor with three arguments
+        Hooman(string name, int age, string gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
     }
     internal class Program
     {
